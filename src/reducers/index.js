219d3d5075d1d6b5dfa3d9ebf1ee157113b829a9
ysKim <znxkznxk1030@gmail.com>
2021-10-09 21:11:30 +0530

    Day - 11 | install react-sparklines

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import reducer_weather from './reducer_weather'
 const rootReducer = combineReducers({
   state: (state = {}) => state,
-  reducer_weather,
+  weather: reducer_weather,
 })
 
 export default rootReducer
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,6 @@
 import { FETCH_WEATHER } from '../actions'
 
-export default function (state = null, action) {
+export default function (state = [], action) {
   console.log('Action: ', action)
   switch (action.type) {
     case FETCH_WEATHER:
