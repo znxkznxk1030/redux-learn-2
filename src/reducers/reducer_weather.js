af43fc20a20453c19a853974bfdcf175ebd15e6c
ysKim <znxkznxk1030@gmail.com>
2021-10-10 18:28:27 +0530

    Day - 13 | redux-promise error handling

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,6 +2,10 @@ import { FETCH_WEATHER } from '../actions'
 
 export default function (state = [], action) {
   console.log('Action: ', action)
+  if (action.error) {
+    return state
+  }
+
   switch (action.type) {
     case FETCH_WEATHER:
       return [action.payload.data, ...state]
